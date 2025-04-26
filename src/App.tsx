778b5f59d8536c41a07512cd2047c242580469f8
KKumanan <krishna.kumanan@gmail.com>
2025-04-26 02:06:43 -0700

    Initial setup: Add Electron + React application with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to Electron + React</h1>
+      <p>Start building your application!</p>
+    </div>
+  );
+};
+
+export default App; 
